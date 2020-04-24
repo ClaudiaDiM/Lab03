42e4d2e30fea65599a6802b887baffe7e19d3f4e
claud <claud@192.168.1.110>
2020-04-24 16:13:00 +0200

    prima parte

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,25 +1,125 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
+
+import it.polito.tdp.spellchecker.model.Dictionary;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class FXMLController {
+	
+	private Dictionary dizionario;
+	private List<String> inputTextList;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private ComboBox<String> boxLingua;
+
+    @FXML
+    private TextArea txtDaCorreggere;
+
+    @FXML
+    private Button spellCheckButton;
+
+    @FXML
+    private TextArea txtCorretto;
+
+    @FXML
+    private Label lblErrori;
+
+    @FXML
+    private Button clearTextButton;
+
+    @FXML
+    private Label lblStato;
+
+    @FXML
+    void doActivation(ActionEvent event) {
+    	
+    	if(boxLingua.getValue()!= null) {
+    		
+    		txtDaCorreggere.setDisable(false);
+    		spellCheckButton.setDisable(false);
+    		clearTextButton.setDisable(false);
+    		txtDaCorreggere.clear();
+    		txtCorretto.clear();
+     }
+    	else {
+    		txtDaCorreggere.setDisable(true);
+    		txtCorretto.setDisable(true);
+    		spellCheckButton.setDisable(true);
+    		clearTextButton.setDisable(true);
+    		txtDaCorreggere.setText("Seleziona una lingua!");
+    		
+    	}
+    	
+
+    }
+
+    @FXML
+    void doClearText(ActionEvent event) {
+    	txtDaCorreggere.clear();
+    	txtCorretto.clear();
+    	lblErrori.setText("Number of Errors:");
+		lblStato.setText("Spell Check Status:");
+
+    }
+
+    @FXML
+    void doSpellCheck(ActionEvent event) {
+    	
+    	txtCorretto.clear();
+    	inputTextList = new ArrayList<String>();
+    	
+    	if(boxLingua.getValue()==null) {
+    		txtDaCorreggere.setText("Seleziona una lingua!");
+    		return;
+    	}
+    	
+    	if(!dizionario.loadDictionary(boxLingua.getValue())) {
+    		txtDaCorreggere.setText("Errore nel caricamento del dizionario!");
+    		return;
+    	}
+    	
+    	String inputText = txtDaCorreggere.getText();
+    	
+    	if(inputText.isEmpty()) {
+    		txtDaCorreggere.setText("Inserire un testo da correggere!");
+			return;
+    	}
+    	
+    	inputText = inputText.replaceAll("\n", " ");
+    	inputText = inputText.replaceAll("[.,\\/#!$%\\^&\\*;:{}=\\-_`~()\\[\\]]", "");
+    	//StringTokenizer mi mette in st le parole che sono divese da spazio
+    	StringTokenizer st = new StringTokenizer(inputText, " ");
+    	while(st.hasMoreTokens()) {
+    		inputTextList.add(st.nextToken());
+    	}
+
+    }
+
+    @FXML
     void initialize() {
+        assert boxLingua != null : "fx:id=\"boxLingua\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtDaCorreggere != null : "fx:id=\"txtDaCorreggere\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert spellCheckButton != null : "fx:id=\"spellCheckButton\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtCorretto != null : "fx:id=\"txtCorretto\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblErrori != null : "fx:id=\"lblErrori\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert clearTextButton != null : "fx:id=\"clearTextButton\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert lblStato != null : "fx:id=\"lblStato\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
-
-
